2586052179d9fd30370b63b3c7a8bac1b2a56c65
JanotLeLapin <joseph300905@gmail.com>
2022-06-06 20:05:30 +0200

    Goodbye Paperclip

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git submodule update --init && ./remap.sh && ./decompile.sh && ./init.sh && ./newApplyPatches.sh && chmod +x ./gradlew && ./gradlew build -x test && ./paperclip.sh
+git submodule update --init && ./remap.sh && ./decompile.sh && ./init.sh && ./newApplyPatches.sh && chmod +x ./gradlew && ./gradlew build -x test && cp ./PaperSpigot-Server/build/libs/paperspigot-server-all.jar ./paper.jar
